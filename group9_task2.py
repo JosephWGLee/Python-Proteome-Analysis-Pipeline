b5c73beeefcb1bb45c81488624350ae29edef6c6
Joseph Lee <148720389+JosephWGLee@users.noreply.github.com>
2025-01-09 13:49:39 +0000

    Add files via upload

---
+++ group9_task2.py
@@ -0,0 +1,103 @@
+import os
+import argparse
+
+def parse_arguments():
+    enzymes = ['t', 'lysc', 'argc', 'v8']
+    parser = argparse.ArgumentParser(description='This script processes a protein sequence dataset, performs enzymatic digestion, and outputs peptides based on specified criteria.')
+    parser.add_argument('--input', '-i', required=True, help='Path to the input file containing protein sequence.')
+    parser.add_argument('--output', '-o', default='task2_output.txt', help='Path to save the output file containing digested peptides.')
+    parser.add_argument('--enzyme', '-e', required=True, choices=enzymes, help='Specify the enzyme for digestion. Choose one of the following: '
+                        'trypsin(t), endoproteinase Lys-C(lysc), endoproteinase Arg-C(argc), v8 proteinase(v8).')
+    parser.add_argument('--peptide_length', '-pm', default=7, type=int, help='Minimum length of peptides to be included in the output. Peptides shorter than this length will be excluded (default: 7 amino acids).')
+    parser.add_argument('--missed_clevage', '-mc', default=1, type=int, help='Maximum number of missed cleavages allowed during digestion. '
+                        'A missed cleavage occurs when the enzyme skips a cleavage site (default: 1).')
+    
+    return parser.parse_args()
+
+def peptide_maker(seq: str, cut_site : str, missed_cleavages: int = 1):
+    """
+    Splits a protein sequence into peptides based on the enzymes provided with support for missed clevages.
+    Args:
+        seq (str) - Protein sequence
+        cut_site (str) - Clevage site
+        missed_clevage (int) - Maximum number of missed clevages allowed
+        
+    Reuturns:
+        list : List of peptides with info about missed clevages
+    """
+    
+    peptides = []
+    temp = 0 # starting index of the current peptide
+    
+    basic_peptides = []  # generating peptides based on the clevage rules
+    for i in range(len(seq)):
+        if seq[i] in cut_site[0]: # clevage site
+            if i+1 >= len(seq) or seq[i+1] != "P": # avoiding cutting if the next amino acid after the clevage site is P
+                basic_peptides.append((temp, i+1))
+                temp = i+1
+    
+    if temp < len(seq): # adding the remaining part
+        basic_peptides.append((temp, len(seq)))
+        
+    for idx, (start, end) in enumerate(basic_peptides):
+        peptides.append((seq[start:end], 0, cut_site[1]))  # Adding the peptide with 0 missed cleavages
+
+        if missed_cleavages > 0 and idx + 1 < len(basic_peptides): # Adding peptides with 1 missed cleavage
+            next_start, next_end = basic_peptides[idx + 1]
+            combined_peptide = seq[start:next_end]
+            peptides.append((combined_peptide, 1, cut_site[1]))
+            
+    return peptides
+        
+def digester_output_maker(header : str, seq_info : set, file : str, peplen : int):
+    """
+    Writes digested peptide sequences to the output file in a specified format.
+    Args:
+        header (str): The FASTA header of the protein sequence.
+                    It identifies the source protein and will be used to label each peptide.
+        seq_info (set): A set of tuples containing:
+                        - peptide_seq (str): The peptide sequence after digestion.
+                        - missed_clevage (int): The number of missed cleavages in the peptide.
+                        - enzyme (str): The enzyme used for digestion.
+        file (str): Path to the output file where digested peptides will be written.
+        peplen (int): Minimum peptide length. Only peptides with lengths greater than or equal
+                    to this value will be included in the output.
+
+    Returns:
+        None, The function is just writing peptide sequences and metadata directly to the output file.
+    """
+    counter = 1
+    with open(file, 'a') as f:
+        for peptide_seq, missed_clevage, enzyme in seq_info:
+            if len(peptide_seq) >= peplen:
+                final_header = f"{header} peptide {counter} {missed_clevage} {enzyme}"
+                f.write(f"{final_header}\n{peptide_seq}\n")
+                counter+=1
+    f.close()
+            
+if __name__ == '__main__':
+    
+    # dictionary of enzymes with their cleavage sites and names
+    enzyme = {
+    't' : ['KR', 'Trypsin'],
+    'lysc' : ['K', 'Endoproteinase Lys-C'],
+    'argc' : ['R', 'Endoproteinase Arg-C'],
+    'v8' : ['E', 'V8 proteinase']
+    }
+    
+    args = parse_arguments()
+    protein_info = {}
+    with open(args.input, 'r') as file: # Read the input protein FASTA file and store headers and sequences
+        temp = ''
+        for i in file.readlines():
+            if ">" in i:
+                temp = i.strip()
+            else:
+                protein_info[temp] = i.strip()
+    
+    if os.path.exists(args.output): # if the output file exists then remove it
+        os.remove(args.output)
+        
+    for header, seq in protein_info.items():
+        peptides = peptide_maker(seq, enzyme[args.enzyme], args.missed_clevage) # Generating peptides for the current protein sequence
+        digester_output_maker(header, peptides, args.output, args.peptide_length) # Writing the peptides to the output file, ensuring minimum peptide length is satisfied
---
+++ proteome.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ORF=$(( RANDOM % 26 + 75 ))
+
+echo -e "\nRunning Task 1: This script takes a nucleotide FASTA file as input and outputs predicted protein ORFs in a FASTA format.\n"
+echo "ORF selected by the user: $ORF"
+python group9_task1.py genome.fasta $ORF task1_output.txt ## TASK 1 COMMAND
+
+# Randomly selecting ENZYME
+Enzymes=("Trypsin" "Endoproteinase Lys-C" "Endoproteinase Arg-C" "V8 proteinase")
+random_enzyme_index=$(( RANDOM % ${#Enzymes[@]} )) # generating the random index between 0 and len of enzymes list - 1 (which is 3 in our case)
+
+random_enzyme=${Enzymes[$random_enzyme_index]} # selecting the random enzyme
+
+# assigning user enzyme as per the selected enzyme
+if [ "$random_enzyme" == "Trypsin" ]; then
+    user_enzyme="t"
+elif [ "$random_enzyme" == "Endoproteinase Lys-C" ]; then
+    user_enzyme="lysc"
+elif [ "$random_enzyme" == "Endoproteinase Arg-C" ]; then
+    user_enzyme="argc"
+elif [ "$random_enzyme" == "V8 proteinase" ]; then
+    user_enzyme="v8"
+fi
+
+# Randomly selecting minimum peptide length and how many missed clevages user want
+
+min_pep=$(( RANDOM % 6 + 5 ))
+missed_clevage=$(( RANDOM % 2))
+
+echo -e "\nRunning Task 2: This script takes protein fasta file and returns a file of smaller peptide sequences\n"
+echo "Enzyme selected by the user: $random_enzyme"
+echo "Missed clevage: $missed_clevage"
+echo "Minimum peptide length: $min_pep"
+python group9_task2.py -i task1_output.txt -o task2_output.txt -e $user_enzyme -pm $min_pep -mc $missed_clevage ## TASK 2 COMMAND
+
+# Randomly generating mass type and charge
+mass_types=("mono" "aver")
+mass_type_idx=$(( RANDOM % ${#mass_types[@]} ))
+
+random_mass_type=${mass_types[$mass_type_idx]}
+
+charge=$(( RANDOM % 5 + 1 ))
+
+echo -e "\nRunning Task 3: This script takes peptide sequence file and returns a table with info of m/z of every peptide sequence\n"
+echo "Mass type: $random_mass_type"
+echo "Charge: $charge"
+python group9_task3.py task2_output.txt -m $random_mass_type -z $charge -o task3_output.txt ## TASK 3 COMMAND
+
+echo -e "\nRunning Task 4: This script takes a table that have info of m/z of every peptide sequence and returns multiple tsv files\n"
+for i in {1..4}; do
+    python group9_task4.py task3_output.txt -m $i ## TASK 4 COMMAND
+    echo
+done
